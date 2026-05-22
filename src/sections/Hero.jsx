7f7d4fc2c2e6072670827d14e000a681a04679cf
DibyanshuSah <dibyanshusah2004@gmail.com>
2026-05-22 23:17:20 +0530

    updated CV

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -30,12 +30,11 @@ const Hero = () => {
         Cybersecurity to create meaningful and impactful solutions.
       </p>
       <a
-        href="https://drive.google.com/uc?export=download&id=12zQDzwDyd2SUEreOKAKBsotXogBuA-YL"
+        href="https://drive.google.com/uc?export=download&id=1qz__B_jxKJ-R1nKvCTTqEaYkiYa0Jfm0"
         className="bg-blue-600 px-7 py-3 rounded-lg font-semibold hover:bg-blue-500 transition"
       >
         Download CV
       </a>
-
     </section>
   );
 };
