1113b92e218470038a396753899769d4bf112441
DibyanshuSah <dibyanshusah2004@gmail.com>
2026-08-06 01:22:34 +0530

    uodated CV

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
         Cybersecurity to create meaningful and impactful solutions.
       </p>
       <a
-        href="https://drive.google.com/uc?export=download&id=1mKZ1Nuj9pSS-EU02svei3ZtDmETOH8Gp"
+        href="https://drive.google.com/uc?export=download&id=1G9vBFc7CKJ0D3zxedZEDNOv9b9yP9Z9g"
         className="bg-blue-600 px-7 py-3 rounded-lg font-semibold hover:bg-blue-500 transition"
       >
         Download CV
